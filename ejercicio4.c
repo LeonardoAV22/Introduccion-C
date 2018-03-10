014c3d1d572743ec0ae8e437bb3ec193c6f6c0ff
Leonardo Alejandre <alejandreleonardov@gmail.com>
2018-03-10 00:41:55 -0600

    Calcular comision y sueldo total

---
+++ ejercicio4.c
@@ -0,0 +1,9 @@
+/*
+A un vendedor le pagan un sueldo base mas el 10% de comision por cada venta, quiere saber de cuanto es su comision y su sueldo al final de mes.
+*/
+
+#include <stdio.h>
+
+main(){
+	float
+}
